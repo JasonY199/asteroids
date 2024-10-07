e1a25b5f77e8ae8cb63f3aa6eb2eba65901ab309
Jason Y <110353075+JasonY199@users.noreply.github.com>
2024-10-07 18:47:33 -0400

    added barebones game loop and drawing of screen

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ from constants import *
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        # allow user to close or quit the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0, 0, 0))  # set screen color black
+        pygame.display.flip()  # refreshes the screen
+
+    # temporary text on screen
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
